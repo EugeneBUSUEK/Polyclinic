da00f0f32e2721a4465d75568d899546e61c13fd
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-01-17 20:39:53 +0300

    moder selffuck

--- src/main/java/ru/rsreu/polyclinic/command/BlockUserCommand.java
+++ src/main/java/ru/rsreu/polyclinic/command/BlockUserCommand.java
@@ -1,5 +1,6 @@
 package ru.rsreu.polyclinic.command;
 
+import ru.rsreu.polyclinic.data.User;
 import ru.rsreu.polyclinic.database.dao.DAOFactory;
 import ru.rsreu.polyclinic.database.dao.UserDAO;
 import ru.rsreu.polyclinic.util.BooleanUtil;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
+import static ru.rsreu.polyclinic.constant.Routes.LOGIN;
 import static ru.rsreu.polyclinic.constant.Routes.MODER;
 
 public class BlockUserCommand extends FrontCommand{
@@ -27,7 +29,13 @@ public class BlockUserCommand extends FrontCommand{
 
     @Override
     public void send() throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
         String login = request.getParameter("username");
+        if (login.equals(user.getLogin())) {
+            request.setAttribute("invalidModerBlock", true);
+            forward(MODER);
+        }
 //        login = login.substring(0,login.length() - 1);
         boolean isBlocked = BooleanUtil.parseBoolean(Integer.parseInt(request.getParameter("isBlocked")));
         this.userDAO.blockUser(login, !isBlocked);
