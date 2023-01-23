d3b3b6e6a667e7dbc5f8c7acad9369c7cc4602b5
luvlaceeeee <nikitan2002@gmail.com>
2023-01-23 03:35:56 +0300

    fgdfbd

--- src/main/java/ru/rsreu/polyclinic/database/dao/VacationRequestsDAO.java
+++ src/main/java/ru/rsreu/polyclinic/database/dao/VacationRequestsDAO.java
@@ -2,11 +2,16 @@ package ru.rsreu.polyclinic.database.dao;
 
 import ru.rsreu.polyclinic.data.Doctor;
 import ru.rsreu.polyclinic.data.RequestSet;
+import ru.rsreu.polyclinic.data.RequestsTableRow;
 import ru.rsreu.polyclinic.data.VacationRequest;
+
+import java.util.List;
 
 public interface VacationRequestsDAO {
 
     RequestSet returnRequestsOfDoctor(Doctor doctor);
+
+    List<RequestsTableRow> returnAllRequests(List<Doctor> doctors);
 
     void updateDoctorRequest(VacationRequest vacationRequest);
 
